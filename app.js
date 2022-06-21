9005bead7c71b3e4ee9167d7ff22baef2412a4bd
Kradnar <pete.daniel.1975@gmail.com>
2022-06-21 15:23:26 +0100

    1st keygen upload

---
+++ app.js
@@ -0,0 +1,26 @@
+presskey = document.getElementById("presskey")
+
+window.setTimeout((welcomeMsg)=> {
+    alert(welcomeMsg);
+}, 1000, "Welcome to my Keycode Generator");
+
+
+
+window.addEventListener("keydown",(keypress)=>{
+    console.log(keypress.code)
+    console.log(keypress.key)
+    console.log(keypress.which)
+    
+    document.getElementById("bigNum").innerHTML= keypress.which;
+    document.getElementById("codeSection").innerHTML= keypress.code;
+    document.getElementById("keySection").innerHTML= keypress.key;
+    document.getElementById("whichSection").innerHTML= keypress.which;
+})
+
+
+function keypress(event) {
+    let code = event.code;
+    let key = event.key;
+    let which = event.which
+
+    }
